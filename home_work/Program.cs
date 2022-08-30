e5cf209a53fba36ffbadc61a429f026dbb35b4c7
Veronika <sapffo@yandex.ru>
2022-08-30 23:17:48 +0300

    домашнее задание

--- home_work/Program.cs
+++ home_work/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+
+//Console.WriteLine(" Введите пятизначное число: ");
+
+//int num = Convert.ToInt32(Console.ReadLine());
+
+//string text = Convert.ToString(num);
+
+
+//if ( text[0] == text[4] && text[1] == text[3])
+//{
+ //Console.WriteLine(" Да!!");
+//}
+ //else
+//{
+ //Console.WriteLine(" Нет!");
+ //}
+
+// Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+
+//int [] array2 = {3, 6, 8};
+//int [] array3 = {2, 1, -7};
+ 
+//double Method1 ( int [] array, int [] array1)
+//{
+   //double num1 = Math.Pow((array1[0] - array[0]), 2);
+   //double num2 = Math.Pow((array1[1] - array[1]), 2);  
+   //double num3 = Math.Pow((array1[2] - array[2]), 2);
+ 
+    
+   //double result = Math.Sqrt(num1 + num2 + num3);
+ 
+  // return result;
+ 
+//}
+     
+ 
+//double result1 = Math.Round(Method1(array2, array3), 2);
+ 
+//Console.WriteLine(result1);
